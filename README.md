571bf67f4174180c337240af48b112dac49826b8
k3tchup <wc373@cornell.edu>
2016-03-05 16:24:58 -0800

    add data for 1.15; partially fix overlapping names

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # VaingloryScripts
+## Overview
 Two scripts that generate hero stats comparison charts in Vainglory.
 
 GetHeroInfo.py, written in Python, retrieves hero stats from the Vainglory website (www.vainglorygame.com), processes the data, then writes the formatted information to HeroStats.txt.
 
 ShowHeroStatsComparisonCharts.m is a MATLAB script that reads HeroStats.txt and plots all the comparison graphs.
 
-Last updated for V1.14 of the game by k3tchup (wc3737).
+## Update Notes
+* 03/05/16 Updated for V1.15 of the game by ketchup (wc3737)
+ * updated HeroStats.txt with V1.15 base stats changes
+ * added partial fix for overlapping names
+* 03/01/16 Updated for V1.14 of the game by k3tchup (wc3737)
+ * initial commits of GetHeroInfo.py, ShowHeroStatsComparisonCharts.m, HeroStats.txt
--- ShowHeroStatsComparisonCharts.m
+++ ShowHeroStatsComparisonCharts.m
@@ -1,5 +1,5 @@
 % Constants
-VERSION = 1.14;
+VERSION = 1.15;
 NUM_STATS = 10;
 NUM_LEVELS = 12;
 GRAPH_WIDTH = 1080;
@@ -12,7 +12,6 @@ names = {};
 stats = double(zeros(1,NUM_STATS));
 increments = double(zeros(1,NUM_STATS));
 counter = 1;
-
 while ~feof(fid)
    line = fgetl(fid);
    line = strsplit(line, DELIM);
@@ -44,12 +43,15 @@ colors(predefColorAssignments(1:length(predefinedColors)),:) = predefinedColors;
 for i = 1:numStats-2
     fig = figure;
     whitebg(fig, 'k')
-    set(fig,'Position', [5 5 GRAPH_WIDTH GRAPH_HEIGHT]);
+    set(fig, 'Position', [0 0 GRAPH_WIDTH GRAPH_HEIGHT]);
+    set(gca, 'OuterPosition', [-0.075 0 0.9 1]) % move plot left in window
     title(['Vainglory ' num2str(VERSION) ' Hero ', titles{i}, ...
         ' Comparison Graph \fontsize{10}by k3tchup'], 'FontSize', 15)
     hold on
     minVal = inf;
     maxVal = -inf;
+    maxes = zeros(numHeroes,1);
+    sortedNames = {}; % a cell array of cell array of names
     for r = 1:numHeroes
         if increments(r,i) > 0
             progress = stats(r,i):increments(r,i):stats(r,i)+increments(r,i)*(NUM_LEVELS-1);
@@ -58,10 +60,46 @@ for i = 1:numStats-2
         end
         minVal = min(minVal, progress(1));
         maxVal = max(maxVal, progress(NUM_LEVELS));
+        maxes(r) = progress(NUM_LEVELS);
+        % graphic commands
         plot(1:NUM_LEVELS, progress, '-o', 'Color', colors(r,:))
-        text(0,progress(1),names{r}, 'Color', colors(r,:), 'FontSize', 10)
-        text(NUM_LEVELS+0.1,progress(NUM_LEVELS),names{r}, 'Color', colors(r,:), 'FontSize', 10)
+        text(0,progress(1),names{r}, 'Color', colors(r,:), 'FontSize', 10) % names at level 1
     end
+    [sorted, idx] = sort(maxes);
+    sortedNamesIdx = 0;
+    sortedColors = colors(idx,:);
+    duplicateIdx = [];
+    newIdx = 1;
+    % if three or more heroes have the same values at level 12, shorten their
+    % display names
+    for s = 1:numHeroes
+        if newIdx == s
+            % compute shouldShortenName
+            newIdx = s+1; % index to next unique value in "sorted"
+            while newIdx <= numHeroes && sorted(newIdx) == sorted(newIdx - 1)
+                newIdx = newIdx + 1;
+            end
+            shouldShortenName = newIdx - s > 2; % if three or more heroes have same stats
+        end
+        if shouldShortenName
+            name = names{idx(s)}(1:2);
+        else
+            name = names{idx(s)};
+        end
+        nameStr = ['{\color[rgb]{' num2str(sortedColors(s,:)) '}' name '}'];
+        if s > 1 && sorted(s) == sorted(s-1)
+            sortedNames{sortedNamesIdx} = [sortedNames{sortedNamesIdx} ', ' nameStr];
+            duplicateIdx = [duplicateIdx s];
+        else
+            sortedNamesIdx = sortedNamesIdx + 1;
+            sortedNames{sortedNamesIdx} = nameStr;
+        end
+    end
+    sorted(duplicateIdx) = []; % dedupe
+    for c = 1:length(sortedNames)
+        text(NUM_LEVELS+0.1,sorted(c),sortedNames{c}, 'FontSize', 10) % names at level 12
+    end
+    % graphic commands
     axis([0 NUM_LEVELS minVal maxVal])
     xlabel('Level')
     ylabel(titles{i});
